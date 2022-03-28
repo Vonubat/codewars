d3a7e0ecabf9bec1a17ed9b25313bc100a08b46d
Tabunov Egor <Vonubat@gmail.com>
2022-03-28 21:33:27 +0300

    feat: solution for "Pair of gloves" task

---
+++ 6kyu/Pair of gloves.js
@@ -0,0 +1,34 @@
+"use strict";
+/*
+Pair of gloves
+Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+
+Given an array describing the color of each glove, return the number of pairs you can constitute, assuming that only gloves of the same color can form pairs.
+
+Examples:
+input = ["red", "green", "red", "blue", "blue"]
+result = 2 (1 red pair + 1 blue pair)
+
+input = ["red", "red", "red", "red", "red", "red"]
+result = 3 (3 red pairs)
+*/
+
+function numberOfPairs(gloves) {
+  let set = new Set();
+  let pairs = 0;
+  gloves.map((item) => {
+    if (!set.has(item)) {
+      set.add(item);
+    } else {
+      set.delete(item);
+      return (pairs += 1);
+    }
+  });
+  return pairs;
+}
+
+console.log(numberOfPairs(["red", "red"])); //1
+console.log(numberOfPairs(["red", "green", "blue"])); //0
+console.log(
+  numberOfPairs(["gray", "black", "purple", "purple", "gray", "black"])
+); //3
