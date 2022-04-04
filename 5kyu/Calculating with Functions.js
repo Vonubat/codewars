5a0c7ca31eeaedee757ec33394573b8963afa76d
Tabunov Egor <Vonubat@gmail.com>
2022-04-04 20:11:05 +0300

    feat: solution for -> "Calculating with Functions" task

--- 5kyu/Calculating with Functions.js
+++ 5kyu/Calculating with Functions.js
@@ -16,18 +16,64 @@ Division should be integer division. For example, this should return 2, not 2.66
 eight(dividedBy(three()));
 */
 
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function expression(number, operation) {
+  if (!operation) return number;
+  return operation(number);
+}
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+function zero(operation) {
+  return expression(0, operation);
+}
+function one(operation) {
+  return expression(1, operation);
+}
+function two(operation) {
+  return expression(2, operation);
+}
+function three(operation) {
+  return expression(3, operation);
+}
+function four(operation) {
+  return expression(4, operation);
+}
+function five(operation) {
+  return expression(5, operation);
+}
+function six(operation) {
+  return expression(6, operation);
+}
+function seven(operation) {
+  return expression(7, operation);
+}
+function eight(operation) {
+  return expression(8, operation);
+}
+function nine(operation) {
+  return expression(9, operation);
+}
+
+function plus(x) {
+  return function (y) {
+    return y + x;
+  };
+}
+function minus(x) {
+  return function (y) {
+    return y - x;
+  };
+}
+function times(x) {
+  return function (y) {
+    return y * x;
+  };
+}
+function dividedBy(x) {
+  return function (y) {
+    return Math.floor(y / x);
+  };
+}
+
+console.log(seven(times(five()))); // must return 35
+console.log(four(plus(nine()))); // must return 13
+console.log(eight(minus(three()))); // must return 5
+console.log(six(dividedBy(two()))); // must return 3
