6ce0ebadf023e4108d15b24d2e04d1cc74ebd1bc
Tabunov Egor <Vonubat@gmail.com>
2022-04-10 00:47:27 +0300

    feat: solution for "Clock in Mirror" task

--- 6kyu/Clock in Mirror.js
+++ 6kyu/Clock in Mirror.js
@@ -28,4 +28,49 @@ There is no 13:20, instead it is 01:20.
 function WhatIsTheTime(timeInMirror) {
   //"05:25" --> "06:35";
   // Happy Coding
+  let arr = timeInMirror.split(":");
+
+  // case for 'hour'
+  if (arr[0] == "12" && arr[1] == "00") {
+    arr[0] = "12";
+  } else if (arr[0] == "11" && arr[1] == "00") {
+    arr[0] = "12";
+  } else if (arr[1] == "00") {
+    arr[0] = `${12 - arr[0]}`;
+  } else if (arr[0] == "12") {
+    arr[0] = "11";
+  } else {
+    arr[0] = `${12 - (+arr[0] + 1)}`;
+  }
+
+  // case for min
+  if (arr[1] == "00") {
+    arr[1] == "00";
+  } else {
+    arr[1] = `${60 - arr[1]}`;
+  }
+
+  // add 0, if is absent
+
+  if (arr[1][1] == undefined) {
+    arr[1] = "0" + arr[1];
+  }
+  if (arr[0][1] == undefined) {
+    arr[0] = "0" + arr[0];
+  }
+
+  // error in hour (00 -> or 12)
+  if (arr[0] == "00") {
+    arr[0] = "12";
+  }
+
+  let result = arr.join(":");
+  console.log(result);
+  return result;
 }
+
+WhatIsTheTime("05:25");
+WhatIsTheTime("01:50 ");
+WhatIsTheTime("11:58");
+WhatIsTheTime("12:01");
+WhatIsTheTime("12:00");
