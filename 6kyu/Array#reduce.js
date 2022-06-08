013931032c9e0e564d9bad163c031876ed2ae65b
Tabunov Egor <Vonubat@gmail.com>
2022-06-08 20:35:18 +0300

    feat: solution for "Array#reduce" task

---
+++ 6kyu/Array#reduce.js
@@ -0,0 +1,42 @@
+'use strict';
+/* In this kata, you must define the Array.reduce method.
+
+I have disabled the pre-existing reduce methods.
+
+Here's how it works:
+
+[1,2,3].reduce( function(sum, next){return sum+next}, 0)
+// => 6
+
+['a','b','a'].reduce( function(obj, elem){if(!obj[elem]) obj[elem] = 0; obj[elem] += 1; return obj}, {})
+// => {'a':2, 'b':1}
+Summary: The reduce method goes through each element of an array, applies the function to whatever the function returned last, and returns the last outcome. On the first iteration, it should pass the initial value to the function, as well as the first element of the array. If no initial value is passed, skip to the first element of the array.
+
+Ruby methods should expect a lambda. */
+
+Array.prototype.reduce = function (process, initial) {
+  let temp = this;
+
+  let result;
+  if (initial) {
+    result = initial;
+  } else if (typeof temp[0] === 'string') {
+    result = '';
+  } else {
+    result = 0;
+  }
+
+  for (let i = 0; i < temp.length; i++) {
+    result = process(result, temp[i]);
+  }
+  console.log(result);
+  return result;
+};
+
+// [1, 2, 3, 4, 5].reduce(function (sum, next) {
+//   return sum + next;
+// }, 5);
+
+// ['a', 'y', '!'].reduce(function (x, y) {
+//   return x + y;
+// }, 'y');
