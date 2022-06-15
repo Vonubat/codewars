b755f93711772735538871c57c0564ef736dd4ea
Tabunov Egor <Vonubat@gmail.com>
2022-06-15 22:45:09 +0300

    feat: solution for "Simple Events.js" task

---
+++ 5kyu/Simple Events.js
@@ -0,0 +1,51 @@
+'use strict';
+/* Your goal is to write an Event constructor function, which can be used to make event objects.
+
+An event object should work like this:
+
+it has a .subscribe() method, which takes a function and stores it as its handler
+it has an .unsubscribe() method, which takes a function and removes it from its handlers
+it has an .emit() method, which takes an arbitrary number of arguments and calls all the stored functions with these arguments
+As this is an elementary example of events, there are some simplifications:
+
+all functions are called with correct arguments (e.g. only functions will be passed to unsubscribe)
+you should not worry about the order of handlers' execution
+the handlers will not attempt to modify an event object (e.g. add or remove handlers)
+the context of handlers' execution is not important
+each handler will be subscribed at most once at any given moment of time. It can still be unsubscribed and then subscribed again
+Also see an example test fixture for suggested usage
+ */
+
+class Event {
+  constructor() {
+    this.storage = [];
+  }
+  subscribe(callback) {
+    this.storage.push(callback);
+  }
+  unsubscribe(callback) {
+    for (let i = 0; i < this.storage.length; i++) {
+      if (this.storage[i] == callback) {
+        this.storage.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  emit(...args) {
+    return this.storage.map((item) => item(...args));
+  }
+}
+
+// let test = new Event();
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// test.subscribe(sum);
+// test.subscribe(sum);
+// test.subscribe(sum);
+// test.subscribe(sum);
+// test.unsubscribe(sum);
+// console.log(test.storage);
+// console.log(test.emit(5, 6));
